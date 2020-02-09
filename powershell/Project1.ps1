01d2a536721379a85cde78de690cc6371b1fc84f
uc-irelanjc <59898804+uc-irelanjc@users.noreply.github.com>
2020-02-09 17:03:05 -0500

    Delete Project1.ps1

--- powershell/Project1.ps1
+++
@@ -1,2 +0,0 @@
-﻿Remove-Item -path "C:\Users\$env:USERNAME\AppData\Local\Google\Chrome\User Data\Default\Cache\*" -Recurse -Force -EA SilentlyContinue -Verbose
-Write-Host "Google Chrome Cache Removal - COMPLETE"
