b2dc5e46b37f8c84ca73fcbff0e3f8b6618b74b2
John Ireland <irelanjc@mail.uc.edu>
2020-02-09 17:03:34 -0500

    adding Project1

--- powershell/Project1.ps1
+++ powershell/Project1.ps1
@@ -1,2 +1,5 @@
-﻿Remove-Item -path "C:\Users\$env:USERNAME\AppData\Local\Google\Chrome\User Data\Default\Cache\*" -Recurse -Force -EA SilentlyContinue -Verbose
+﻿#What it does: This is a script that clears the cache from Chrome
+#Output: You should see each cache being deleted because of -Verbose. And "Google Chrome Cache Removal - COMPLETE" to let you know the script is done.
+#How to run it: To run it copy the script into your own powershell and run it. To check if the script worked check the file path "C:\Users\YourUser\AppData\Local\Google\Chrome\User Data\Default\Cache\*". The cache file should be empty.
+Remove-Item -path "C:\Users\$env:USERNAME\AppData\Local\Google\Chrome\User Data\Default\Cache\*" -Recurse -Force -EA SilentlyContinue -Verbose
 Write-Host "Google Chrome Cache Removal - COMPLETE"
