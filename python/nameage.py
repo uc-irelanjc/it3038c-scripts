c243e1aae4d4929dd40201ecbec46114c0bb5281
John Ireland <irelanjc@mail.uc.edu>
2020-02-11 19:03:05 -0500

    Adding Python

---
+++ python/nameage.py
@@ -0,0 +1,11 @@
+import time
+start_time = time.time()
+print('What is your name?')
+myName = input()
+print('Hello, ' + myName + '. That is a good name. How old are you?')
+myAge = input()
+programAge = int(time.time() - start_time)
+print('%s? That is funny, I am only a %s seconds old.' % (myAge, programAge))
+print("I wish I was %s years old" % (myAge * 2))
+time.sleep(3)
+print("I am tired. I go to sleep now.")
